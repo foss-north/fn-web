bb4daa265d6ac1abc9a50003e0e395a713456b60
Johan Thelin <e8johan@gmail.com>
2019-06-25 16:32:15 +0200

    Initial commit

---
+++ code/skin_settings.py
@@ -0,0 +1,15 @@
+ORG_NAME="FOSS North"
+ORG_SHORTNAME="FOSSNORTH"
+
+ENABLE_MEMBERSHIP=False
+ENABLE_ELECTIONS=False
+ENABLE_AUTO_ACCOUNTING=True
+
+
+DEFAULT_EMAIL="johan@foss-gbg.se"
+SERVER_EMAIL="johan@foss-gbg.se"
+TREASURER_EMAIL="johan@foss-gbg.se"
+INVOICE_SENDER_EMAIL="johan@foss-gbg.se"
+SCHEDULED_JOBS_EMAIL="johan@foss-gbg.se"
+
+ENABLE_OAUTH_AUTH=True
